52a81a02bb45e770ad6a6a718919d99b7b6eeab0
Alonso <ajimenez@siccob.com.mx>
2025-02-01 00:58:50 -0600

    Add between on daily spend report

--- app/Http/Controllers/Report/CardCloud/DailyConsume.php
+++ app/Http/Controllers/Report/CardCloud/DailyConsume.php
@@ -60,41 +60,61 @@ class DailyConsume extends Controller
 
     public function index(Request $request)
     {
-        $this->validate($request, [
-            'date' => 'required|date_format:Y-m-d',
-        ], [
-            'date.required' => 'The date field is required.',
-            'date.date_format' => 'The date field must be in the format Y-m-d.',
-        ]);
-
         try {
+
+            if ($request->has("from")) {
+                $this->validate($request, [
+                    'from' => 'required|date_format:Y-m-d',
+                    'to' => 'required|date_format:Y-m-d',
+                ], [
+                    'from.required' => "La fecha inicial es requerida.",
+                    'from.date_format' => "La fecha inicial debe tener el formato Y-m-d.",
+                    'to.required' => "La fecha final es requerida.",
+                    'to.date_format' => "La fecha final debe tener el formato Y-m-d.",
+                ]);
+
+                $dates = [
+                    'from' => $request->from,
+                    'to' => $request->to,
+                ];
+            } else {
+                $this->validate($request, [
+                    'date' => 'required|date_format:Y-m-d',
+                ], [
+                    'date.required' => 'La fecha es requerida.',
+                    'date.date_format' => 'La fecha debe tener el formato Y-m-d.',
+                ]);
+
+                $dates = [
+                    'date' => $request->date,
+                ];
+            }
+
             Validate::userProfile([7, 9], $request->attributes->get('jwt')->profileId);
+
+            $companies = CompaniesByUser::get($request->attributes->get('jwt'));
+
+            if ($companies->count() == 0) {
+                throw new \Exception("El usuario no tiene empresas asociadas");
+            }
+
+            $companiesArrayIds = $companies->pluck('CompanyId')->toArray();
+
+            $client = new \GuzzleHttp\Client();
+
+            $response = $client->request('GET', env('CARD_CLOUD_BASE_URL') . '/api/v1/reports/daily_spend', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . CardCloudApi::getToken($request->attributes->get('jwt')->id),
+                ],
+                'body' => json_encode(array_merge([
+                    'companies' => $companiesArrayIds
+                ], $dates)),
+            ]);
+
+            return response($response->getBody()->getContents());
         } catch (\Exception $e) {
-            return response($e->getMessage(), 400);
+            return self::basicError($e->getMessage());
         }
-
-        $companies = CompaniesByUser::get($request->attributes->get('jwt'));
-
-        if ($companies->count() == 0) {
-            return response('User does not have any companies associated', 400);
-        }
-
-        $companiesArrayIds = $companies->pluck('CompanyId')->toArray();
-
-        $client = new \GuzzleHttp\Client();
-
-        $response = $client->request('GET', env('CARD_CLOUD_BASE_URL') . '/api/v1/reports/daily_spend', [
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . CardCloudApi::getToken($request->attributes->get('jwt')->id),
-            ],
-            'body' => json_encode([
-                'companies' => $companiesArrayIds,
-                'date' => $request->date,
-            ]),
-        ]);
-
-        return response($response->getBody()->getContents());
     }
-
 }
