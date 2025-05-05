c19516f532c67eca2ad6aad67317d6b7419da77c
Alonso Jiménez <ajimmenezz@gmail.com>
2025-05-05 14:34:16 -0600

    Migrations

---
+++ database/migrations/2025_05_05_171854_add_t_speicloud_authorization_rules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_speicloud_authorization_rules', function (Blueprint $table) {
+            $table->id('Id');
+            $table->integer('RuleType')->default(1)->index()->comment('1: Spei Out, 2: Spei In');
+            $table->decimal('Amount', 10, 2)->nullable();
+            $table->integer('DailyMovementsLimit')->nullable();
+            $table->integer('MonthlyMovementsLimit')->nullable();
+            $table->integer('Priority')->default(0)->comment('Bigger number means higher priority around the other rules');
+            $table->string('CreatedBy', 36)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_speicloud_authorization_rules');
+    }
+};
