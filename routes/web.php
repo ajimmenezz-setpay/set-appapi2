e738f4b2df3ca02840dc676136b1a33ae9e92ed8
Alonso <ajimenez@siccob.com.mx>
2025-07-30 22:50:07 -0600

    Enable documentation en read

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['version' => '2.0.1']);
 });
+
+Route::get('/docs/conector', function () {
+    return view('docs.conector');
+});
