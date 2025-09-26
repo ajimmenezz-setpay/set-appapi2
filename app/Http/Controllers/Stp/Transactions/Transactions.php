71865844bb02e286ac85b96597fa12b5e7690046
Alonso <ajimenez@siccob.com.mx>
2025-09-26 12:58:43 -0600

    Fix Spei Ins

--- app/Http/Controllers/Stp/Transactions/Transactions.php
+++ app/Http/Controllers/Stp/Transactions/Transactions.php
@@ -23,64 +23,71 @@ class Transactions extends Controller
 
     public static function fixBalances()
     {
-        $companies = [];
-        foreach (CompanyProjection::all() as $company) {
-            $balance = 0;
-            $services = json_decode($company->Services);
-            foreach ($services as $service) {
-                if ($service->type != 4) {
-                    continue;
-                }
-                $companies[$company->Id] = [
-                    'id' => $company->Id,
-                    'name' => $company->TradeName,
-                    'balance' => $company->Balance,
-                    'clabe' => $service->bankAccountNumber,
-                    'movements' => self::getAccountMovements($service->bankAccountNumber)
-                ];
-            }
-
-            if (!isset($companies[$company->Id])) {
-                continue;
-            }
-
-            if ($companies[$company->Id]['movements']->count() == 0) {
-                unset($companies[$company->Id]);
-            } else {
-                foreach ($companies[$company->Id]['movements'] as $movement) {
-                    $commissions = null;
-                    if ($movement->Commissions && $movement->Commissions != "") {
-                        $commissions = json_decode($movement->Commissions);
+        try {
+            $companies = [];
+            foreach (CompanyProjection::all() as $company) {
+                $balance = 0;
+                $services = json_decode($company->Services);
+                foreach ($services as $service) {
+                    if ($service->type != 4 || !isset($service->bankAccountNumber) || $service->bankAccountNumber == "") {
+                        continue;
                     }
 
-                    $total_amount = is_null($commissions) ? $movement->Amount : $commissions->total;
-                    if ($movement->TypeId == 1) $total_amount = $total_amount * -1;
-
-                    $balance += $total_amount;
-
-                    StpTransaction::where('Id', $movement->Id)->update([
-                        ($movement->TypeId == 1 ? 'SourceBalance' : 'DestinationBalance') => $balance
-                    ]);
+                    $companies[$company->Id] = [
+                        'id' => $company->Id,
+                        'name' => $company->TradeName,
+                        'balance' => $company->Balance,
+                        'clabe' => $service->bankAccountNumber,
+                        'movements' => self::getAccountMovements($service->bankAccountNumber)
+                    ];
                 }
-                $companies[$company->Id]['new_balance'] = number_format($balance, 2, '.', '');
-                unset($companies[$company->Id]['movements']);
+
+                if (!isset($companies[$company->Id])) {
+                    continue;
+                }
+
+                if ($companies[$company->Id]['movements']->count() == 0) {
+                    unset($companies[$company->Id]);
+                } else {
+                    foreach ($companies[$company->Id]['movements'] as $movement) {
+                        $commissions = null;
+                        if ($movement->Commissions && $movement->Commissions != "") {
+                            $commissions = json_decode($movement->Commissions);
+                        }
+
+                        $total_amount = is_null($commissions) ? $movement->Amount : $commissions->total;
+                        if ($movement->TypeId == 1) $total_amount = $total_amount * -1;
+
+                        $balance += $total_amount;
+
+                        StpTransaction::where('Id', $movement->Id)->update([
+                            ($movement->TypeId == 1 ? 'SourceBalance' : 'DestinationBalance') => $balance
+                        ]);
+                    }
+                    $companies[$company->Id]['new_balance'] = number_format($balance, 2, '.', '');
+                    unset($companies[$company->Id]['movements']);
+                }
+
+                CompanyProjection::where('Id', $company->Id)
+                    ->update([
+                        'Balance' => $balance
+                    ]);
+
+                Company::where('Id', $company->Id)
+                    ->update([
+                        'Balance' => $balance
+                    ]);
             }
 
-            CompanyProjection::where('Id', $company->Id)
-                ->update([
-                    'Balance' => $balance
-                ]);
-
-            Company::where('Id', $company->Id)
-                ->update([
-                    'Balance' => $balance
-                ]);
+            return [
+                'total' => count($companies),
+                'companies' => $companies
+            ];
+        } catch (\Exception $e) {
+            return [
+                'error' => $e->getMessage()
+            ];
         }
-
-        return [
-            'total' => count($companies),
-            'companies' => $companies
-        ];
     }
 
     public static function getAccountMovements($bankAccountNumber)
