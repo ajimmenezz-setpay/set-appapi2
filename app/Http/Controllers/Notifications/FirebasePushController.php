03d112307490c8f84aeca52ee8979345d6c75dd8
Alonso <ajimenez@siccob.com.mx>
2025-10-27 21:28:27 -0600

    Add documentation - Asociate Firebase Token with user

--- app/Http/Controllers/Notifications/FirebasePushController.php
+++ app/Http/Controllers/Notifications/FirebasePushController.php
@@ -10,6 +10,48 @@ use Illuminate\Support\Facades\Http;
 
 class FirebasePushController extends Controller
 {
+    /**
+     * @OA\Post(
+     *      path="/api/users/firebase-token/asociate",
+     *      summary="Asociar token de dispositivo Firebase al usuario autenticado",
+     *      tags={"Firebase Push Notifications"},
+     *      description="Asocia el token de dispositivo Firebase al usuario autenticado para recibir notificaciones push.",
+     *      operationId="asociateDeviceToken",
+     *      security={{"bearerAuth":{}}},
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"firebase_token"},
+     *              @OA\Property(property="firebase_token", type="string", maxLength=512, example="cUTUYpXhTYi397sCPb4MzK:APA91bHcjOxgOLiS5CScaBw4pUf5TMakie-b-aq2wwQ8l0O6-Nqs8aHpq9xL8GeZBh7e5YD35DwxDWpxoEac52ZKU3Vq0DeOVF031XoaBqbPnjsCkEVfEGo")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Token del dispositivo asociado correctamente",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Token del dispositivo asociado correctamente")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=400,
+     *          description="Error al asociar el token del dispositivo",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="error", type="string", example="Error al asociar el token del dispositivo: [detalles del error]")
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=401,
+     *          description="No autorizado",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="error", type="string", example="No autorizado")
+     *          )
+     *     )
+     * )
+     */
     public function asociateDeviceToken(Request $request)
     {
         try {
@@ -50,7 +92,7 @@ class FirebasePushController extends Controller
             if (!$response->successful()) {
                 Log::error('❌ Error al enviar la notificación push');
                 Log::error('Respuesta: ' . $response->body());
-            }else{
+            } else {
                 Log::info('✅ Notificación push enviada correctamente');
                 Log::info('Respuesta: ' . $response->body());
             }
