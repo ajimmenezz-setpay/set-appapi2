0b0a6a3db2a141ffc112edd1d90010ef4aa2f103
Alonso <ajimenez@siccob.com.mx>
2025-12-06 11:53:42 -0600

    Remove Api Logger

--- app/Http/Middleware/ApiLogger.php
+++ app/Http/Middleware/ApiLogger.php
@@ -11,33 +11,33 @@ class ApiLogger
     public function handle(Request $request, Closure $next)
     {
         // Iniciar cronómetro
-        $start = microtime(true);
+        // $start = microtime(true);
 
         // Procesar request
         $response = $next($request);
 
         // Detener cronómetro
-        $end = microtime(true);
+        // $end = microtime(true);
 
         // Convertir a milisegundos
-        $executionTime = ($end - $start) * 1000;
+        // $executionTime = ($end - $start) * 1000;
 
         // Obtener info del JWT
-        $jwt = $request->attributes->get('jwt');
-        $userId = $jwt->id ?? null;
+        // $jwt = $request->attributes->get('jwt');
+        // $userId = $jwt->id ?? null;
 
         // Guardar log
-        ApiLog::create([
-            'user_id'           => $userId,
-            'method'            => $request->method(),
-            'url'               => $request->fullUrl(),
-            'ip'                => $request->ip(),
-            'request_headers'   => json_encode($request->headers->all()),
-            'request_body'      => json_encode($request->all()),
-            'response_code'     => $response->getStatusCode(),
-            'response_body'     => $response->getContent(),
-            'execution_time_ms' => number_format($executionTime, 2, '.', ''),
-        ]);
+        // ApiLog::create([
+        //     'user_id'           => $userId,
+        //     'method'            => $request->method(),
+        //     'url'               => $request->fullUrl(),
+        //     'ip'                => $request->ip(),
+        //     'request_headers'   => json_encode($request->headers->all()),
+        //     'request_body'      => json_encode($request->all()),
+        //     'response_code'     => $response->getStatusCode(),
+        //     'response_body'     => $response->getContent(),
+        //     'execution_time_ms' => number_format($executionTime, 2, '.', ''),
+        // ]);
 
         return $response;
     }
